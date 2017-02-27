5f015634da80462960ca0b4aa488deb7f58e8617
Ajay Prasadh Viswanathan <ajay@Ajays-MacBook-Pro.local>
2017-02-27 00:06:20 +0000

    LOL

--- app/conv_model/prediction.py
+++ app/conv_model/prediction.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics, preprocessing, svm
 from sklearn.metrics import confusion_matrix
 import random
+from sklearn.externals import joblib
 # RESTORE TENSORFLOW MODELS
 # READ AND PROCESS IMAGE
 # PREDICT OUTPUT
@@ -32,6 +33,14 @@ def predict_class(img):
 	tf.set_random_seed(random_state)
 	y = {0:'backpacks', 1: 'bags', 2:'luggage',3:'travel_accessoires'}
 
+	luggage_inverse = {0: 'american-tourister', 1: 'eastpak', 2: 'epic', 3: 'hartmann', 4: 'lufthansa', 5: 'montblanc', 6: 'rimowa', 7: 'samsonite', 8: 'stratic', 9: 'thule', 10: 'titan', 11: 'travelite', 12: 'victorinox', 13: 'wenger'}
+	bag_inverse = {0: 'bag-to-life', 1: 'braun', 2: 'eastpak', 3: 'harolds', 4: 'hartmann', 5: 'leonhard-heyden', 6: 'lufthansa', 7: 'montblanc', 8: 'rimowa', 9: 'samsonite', 10: 'travelite', 11: 'unnamed', 12: 'victorinox', 13: 'wenger'}
+	backpack_inverse = {0: 'aigner', 1: 'bag-to-life', 2: 'braun', 3: 'eastpak', 4: 'laessig', 5: 'lufthansa', 6: 'samsonite', 7: 'tatonka', 8: 'thule', 9: 'travelite'}
+	luggage = joblib.load(rel_path + '/model4/luggage.pkl')
+	bag = joblib.load(rel_path + '/model4/bags.pkl')
+	backpacks = joblib.load(rel_path + '/model4/backpacks.pkl')	
+
+
 	# READ AND PROCESS IMAGE
 	img = np.reshape(img,(64,64,4))
 	img = img[:,:,:3]
@@ -42,8 +51,14 @@ def predict_class(img):
 	# CALCULUATE PREDICTION
 	pred = y_conv.eval(session = sess,feed_dict={x:img})
 	sess.close()
-	
+	brand = ''
 	for i in range(4):
 		if(pred[0][i] == max(pred[0])):
-			return y[i]
+			if(y[i] == 'luggage'):
+                        	brand = luggage_inverse[luggage.predict(img)[0]]
+                	if(y[i] == 'bags'):
+                        	brand = bag_inverse[bag.predict(img)[0]]
+                	if(y[i] == 'backpacks'):
+                        	brand = backpack_inverse[backpacks.predict(img)[0]]
+			return 'category = ' + y[i] + ' brand = ' + brand
 
